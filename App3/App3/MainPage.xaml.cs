85b71df8eeaf65e3aa3e8a244a136aca0d8cf1a1
antin <antin@DESKTOP-DG6RP0F>
2021-05-19 20:18:16 +0300

    Done hello world page and prepared resume page start page with hello world and and second page prepared for resume

---
+++ App3/App3/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App3.Views;
+
+namespace App3
+{
+    public partial class MainPage : Xamarin.Forms.Shell
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
